3f0c4827cbf183c604b9b5b8eb158ab48780325b
jolugara <jolugara2000@gmail.com>
2022-01-18 19:16:43 +0100

    Terminado tirar los dados y en proceso de guardar los anteriores dados

---
+++ main.py
@@ -0,0 +1,39 @@
+import random
+
+def TirarDados():
+    Dado1 = random.randrange(10)
+    Dado2 = random.randrange(10)
+    Dado3 = random.randrange(10)
+    print("🎲🎲 Dados 🎲🎲")
+    print(f"➡️Dado1: {Dado1}, Dado2: {Dado2}, Dado3: {Dado3}  ⬅️")
+    SumarDados = Dado1+Dado2+Dado3
+    print(f"1️⃣Primera suma de dados: {SumarDados}")
+    x = [int(a) for a in str(SumarDados)]
+    print(f"2️⃣Segunda suma de dados: {x}")
+    SumaTotal = sum(x)
+    print(f"#️⃣Suma Final: {SumaTotal}")
+    return SumaTotal
+
+
+ResultadoDados = TirarDados()
+
+def GuarderDados():
+    DadosGuardados = ResultadoDados
+    return DadosGuardados
+
+
+def ComprobarDados():
+    if ResultadoDados <= 4:
+        print("Avanzas casilla")
+    else:
+        print("Te quedas en la misma casilla")
+print(ComprobarDados())
+
+
+def MoverseEnTablero():
+    LugarTablero = 0
+    while(ResultadoDados <= 4):
+        MoverFicha = LugarTablero + ResultadoDados
+        print(MoverFicha)
+        return MoverFicha
+print(MoverseEnTablero())
