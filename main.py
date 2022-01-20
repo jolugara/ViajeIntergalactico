44f5e61812d69e1cc2e8013cf2a60fb2d1b4d8ba
jolugara <jolugara2000@gmail.com>
2022-01-20 20:31:25 +0100

    commit v2

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import random
 
 def TirarDados():
-    Dado1 = random.randrange(10)
-    Dado2 = random.randrange(10)
-    Dado3 = random.randrange(10)
-    print("🎲🎲 Dados 🎲🎲")
-    print(f"➡️Dado1: {Dado1}, Dado2: {Dado2}, Dado3: {Dado3}  ⬅️")
-    SumarDados = Dado1+Dado2+Dado3
-    print(f"1️⃣Primera suma de dados: {SumarDados}")
-    x = [int(a) for a in str(SumarDados)]
-    print(f"2️⃣Segunda suma de dados: {x}")
-    SumaTotal = sum(x)
-    print(f"#️⃣Suma Final: {SumaTotal}")
-    return SumaTotal
+    SumaTotal = 42
+    while (SumaTotal >= 42):
+        Dado1 = random.randrange(10)
+        Dado2 = random.randrange(10)
+        Dado3 = random.randrange(10)
+        print("🎲🎲 Dados 🎲🎲")
+        print(f"➡️Dado1: {Dado1}, Dado2: {Dado2}, Dado3: {Dado3}  ⬅️")
+        SumarDados = Dado1+Dado2+Dado3
+        print(f"1️⃣Primera suma de dados: {SumarDados}")
+        x = [int(a) for a in str(SumarDados)]
+        print(f"2️⃣Segunda suma de dados: {x}")
+        SumaTotal = sum(x)
+        print(f"#️⃣Suma Final: {SumaTotal}")
+        return SumaTotal
+    print("fin")
 
 
 ResultadoDados = TirarDados()
@@ -29,7 +32,7 @@ def ComprobarDados():
         print("Te quedas en la misma casilla")
 print(ComprobarDados())
 
-
+''''
 def MoverseEnTablero():
     LugarTablero = 0
     while(ResultadoDados <= 4):
@@ -37,3 +40,10 @@ def MoverseEnTablero():
         print(MoverFicha)
         return MoverFicha
 print(MoverseEnTablero())
+'''''
+CasillaMaxima = 42
+def iniciojuego():
+    while (ResultadoDados >= CasillaMaxima):
+        print(TirarDados())
+
+
